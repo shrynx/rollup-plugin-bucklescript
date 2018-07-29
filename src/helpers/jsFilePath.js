82d0a11164b1a84cdfc18f7edd1ad508ecb0b976
Neil Kistner <neil.kistner@gmail.com>
2018-07-29 08:27:57 -0500

    Use suffix from bsconfig in jsFilePath
    The plugin was reading the correct suffix and passing it to
    `jsFilePath`, but then it was never used and was hard coded to `.js`.

--- src/helpers/jsFilePath.js
+++ src/helpers/jsFilePath.js
@@ -1,11 +1,11 @@
 import * as path from 'path';
 
-const jsFilePath = (buildDir, moduleDir, resourcePath, inSource) => {
+const jsFilePath = (buildDir, moduleDir, resourcePath, inSource, suffix) => {
   const outputDir = 'lib';
   const fileNameRegex = /\.(ml|re)$/;
 
   const mlFileName = resourcePath.replace(buildDir, '');
-  const jsFileName = mlFileName.replace(fileNameRegex, '.js');
+  const jsFileName = mlFileName.replace(fileNameRegex, suffix);
 
   if (inSource) {
     return path.join(buildDir, jsFileName);
